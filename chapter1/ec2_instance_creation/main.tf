3a49e331cef7c04c7f3f02638c08e289461a08f6
samarth <chindesamarth@gmail.com>
2026-05-20 09:17:47 +0000

    adding multiple instances

--- chapter1/ec2_instance_creation/main.tf
+++ chapter1/ec2_instance_creation/main.tf
@@ -13,3 +13,27 @@ resource "aws_instance" "fist_instance" {
       name = "first instance"
     }
 }
+
+resource "aws_instance" "second_instance" {
+    ami = "ami-07a00cf47dbbc844c"
+    instance_type = "t3.micro"
+    subnet_id = "subnet-0adf927845a1e17dc"
+    key_name = "default"
+    security_groups = ["sg-0f8a67fc4cc8dc542"]
+
+    tags = {
+      name = "second instance"
+    }
+}
+
+resource "aws_instance" "third_instance" {
+    ami = "ami-07a00cf47dbbc844c"
+    instance_type = "t3.micro"
+    subnet_id = "subnet-0adf927845a1e17dc"
+    key_name = "default"
+    security_groups = ["sg-0f8a67fc4cc8dc542"]
+
+    tags = {
+      name = "thid instance"
+    }
+}
