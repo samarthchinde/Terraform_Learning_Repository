e158ae80a9a9d4dbc602cf7b6fde7dfd2985995b
samarth <chindesamarth@gmail.com>
2026-05-20 09:23:02 +0000

    adding the tages for better understanding of the code

--- chapter1/ec2_instance_creation/main.tf
+++ chapter1/ec2_instance_creation/main.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-    region = "ap-south-1"
+    region = "ap-south-1"           # adding region to provider block (opional)
 }
 
 resource "aws_instance" "fist_instance" {
-    ami = "ami-07a00cf47dbbc844c"
+    ami = "ami-07a00cf47dbbc844c"           # ami id for launching an instance in ap-south-1 region
     instance_type = "t3.micro"
-    subnet_id = "subnet-0adf927845a1e17dc"
+    subnet_id = "subnet-0adf927845a1e17dc"   # subnet id for launching an instance in ap-south-1 region
     key_name = "default"
-    security_groups = ["sg-0f8a67fc4cc8dc542"]
+    security_groups = ["sg-0f8a67fc4cc8dc542"]  # add seurity group to the instance (add subnet id)
 
-    tags = {
+    tags = {                                  # adding tags to the instance
       name = "first instance"
     }
 }
