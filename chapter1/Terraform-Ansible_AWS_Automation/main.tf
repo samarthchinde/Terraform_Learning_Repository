3f247021a3494ae0228cf99a8d925f60971c73bd
samarth <chindesamarth@gmail.com>
2026-05-20 16:46:56 +0000

    main verion of the main.tf file

---
+++ chapter1/Terraform-Ansible_AWS_Automation/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+    region = "ap-south-1"          
+}
+
+resource "aws_instance" "main_server" {
+    ami = "ami-07a00cf47dbbc844c"
+    subnet_id = "subnet-0adf927845a1e17dc"
+    instance_type = "t3.micro"
+    key_name = "default"
+    security_groups = [ "sg-0f8a67fc4cc8dc542" ]
+
+    tags = {
+        name = "main_server"
+    }
+}
+
+resource "aws_instance" "first_server" {
+    ami = "ami-07a00cf47dbbc844c"
+    subnet_id = "subnet-0adf927845a1e17dc"
+    instance_type = "t3.micro"
+    key_name = "default"
+    security_groups = [ "sg-0f8a67fc4cc8dc542" ]
+
+    tags = {
+        name = "first_server"
+    }
+}
+
+resource "aws_instance" "second_server" {
+    ami = "ami-07a00cf47dbbc844c"
+    subnet_id = "subnet-0adf927845a1e17dc"
+    instance_type = "t3.micro"
+    key_name = "default"
+    security_groups = [ "sg-0f8a67fc4cc8dc542" ]
+
+    tags = {
+        name = "second_server"
+    }
+}
