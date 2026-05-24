4e42b17a3c280ace6a6b715545c55d6e4b1baf2e
samarth <chindesamarth@gmail.com>
2026-05-24 03:17:30 +0000

    changed key value pair

--- chapter1/Terraform-Ansible_AWS_Automation_project/main.tf
+++ chapter1/Terraform-Ansible_AWS_Automation_project/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "Third_server" {
     ami = "ami-07a00cf47dbbc844c"
     subnet_id = "subnet-0adf927845a1e17dc"
     instance_type = "t3.micro"
-    key_name = "default"
+    key_name = "demo"
     security_groups = [ "sg-0f8a67fc4cc8dc542" ]
 
     tags = {
@@ -18,7 +18,7 @@ resource "aws_instance" "first_server" {
     ami = "ami-07a00cf47dbbc844c"
     subnet_id = "subnet-0adf927845a1e17dc"
     instance_type = "t3.micro"
-    key_name = "default"
+    key_name = "demo"
     security_groups = [ "sg-0f8a67fc4cc8dc542" ]
 
     tags = {
@@ -30,7 +30,7 @@ resource "aws_instance" "second_server" {
     ami = "ami-07a00cf47dbbc844c"
     subnet_id = "subnet-0adf927845a1e17dc"
     instance_type = "t3.micro"
-    key_name = "default"
+    key_name = "demo"
     security_groups = [ "sg-0f8a67fc4cc8dc542" ]
 
     tags = {
