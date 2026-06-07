d6c405fe1edafb9e480c3ed942cb0c1f4d5a1d73
samarth <chindesamarth@gmail.com>
2026-06-07 15:22:47 +0000

    providers and variables moved in respective files

--- chapter1/newproject/main.tf
+++ chapter1/newproject/main.tf
@@ -1,30 +1,3 @@
-provider "aws" {
-    alias  = "mumbai"
-    region = "ap-south-1"
-}
-
-provider "aws" {
-    alias  = "virginia"
-    region = "us-east-1"
-}
-
-variable "ami_id_mumbai" {
-    description = "The AMI ID to use for the instance"
-    type = string
-    default = "ami-07a00cf47dbbc844c"
-}
-
-variable "instance_type" {
-    description = "The type of instance to launch"
-    type = string
-    default = "t3.micro"
-}
-
-variable "ami_id_virginia" {
-    description = "The AMI ID to use for the instance"
-    type = string
-    default = "ami-091138d0f0d41ff90"
-}
 
 resource "aws_instance" "instance_one" {
     provider = aws.mumbai
